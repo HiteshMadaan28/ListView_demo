0d6a04db7b20f714bc2e83c0ed5c5eec2085ee6d
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-05-05 20:03:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/listview_demo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.listview_demo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+    String[] listitem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.user_list);
+        textView = findViewById(R.id.textView);
+        listitem = getResources().getStringArray(R.array.array_technology);
+
+        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,listitem);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
